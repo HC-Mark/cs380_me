2561582607be0d7ed1b5ad7103fc3b7a3f19090c
Tianming Xu <txu1@haverford.edu>
2017-04-06 16:55:53 -0400

    Add files via upload
    I got concatMap

--- hw07/Assign7.hs
+++ hw07/Assign7.hs
@@ -1,7 +1,7 @@
-  {-# LANGUAGE GADTs, TypeInType, StandaloneDeriving, EmptyCase,TypeOperators,TypeFamilies, UndecidableInstances,AllowAmbiguousTypes #-}
+{-# LANGUAGE GADTs, TypeInType, StandaloneDeriving, EmptyCase,TypeOperators,TypeFamilies, UndecidableInstances,AllowAmbiguousTypes #-}
  {-# OPTIONS_GHC -Wincomplete-patterns #-}
 
-module Fin where
+module Assign7 where
 
 import Data.Kind ( Type )
 import Prelude hiding ( (++),concat,foldr,concatMap )
@@ -22,9 +22,10 @@ data Fin :: Nat -> Type where
 
 data EVec :: (Nat -> Type) -> Type -> Type where
   EVec :: p n -> Vec n a -> EVec p a
+
 instance (Show a) => Show (EVec c a) where
  show (EVec _ v) = show v
- 
+
 data AlwaysTrue :: Nat -> Type where
   Always :: AlwaysTrue n
 
@@ -36,6 +37,8 @@ data VecList :: [Nat] -> Type -> Type where
    VLNil :: VecList '[] a
    (:>>) :: Vec n a -> VecList ns a -> VecList (n ': ns) a
 infixr 5 :>>
+
+deriving instance Show a => Show (VecList ns a)
 
 type family (a :: Nat) + (b :: Nat) :: Nat where
    Zero   + b = b
@@ -58,13 +61,14 @@ concat (xs :>> xss) = xs ++ concat xss
 
 --examples
 stuffs = (1 :> 2 :> Nil) :>> (3 :> Nil) :>> (4 :> 5 :> 6 :> Nil) :>> VLNil
-
+stuff = 3 :> 5:> 8:> Nil
 --helper function
 foldr :: (a -> b -> b) -> b -> Vec n a -> b
 foldr _ x Nil = x
 foldr f x ( y :> ys ) = (f y (foldr f x ys))
 
 --1. concatMap
+{-
 type family NatToList (n:: Nat) (m:: Nat) :: [Nat] where
   NatToList n Zero      = '[]
   NatToList n (Succ m') = n : NatToList n m'
@@ -75,3 +79,11 @@ concatMap f (x:>xs) = concat $ (f x) :>> storeInVecList f xs
      where
        storeInVecList :: (a -> Vec n b) -> Vec m a -> VecList (NatToList n m) b
        storeInVecList = undefined
+-}
+
+concatMap :: (a ->EVec AlwaysTrue b) -> Vec n a -> EVec AlwaysTrue b
+concatMap _ Nil = EVec Always Nil
+concatMap f (x :> xs) = case concatMap f xs of EVec Always xs' -> case f x of EVec Always ys' -> EVec Always (xs'++ys')
+
+testDouble :: a -> EVec AlwaysTrue a
+testDouble n = EVec Always (n :> n :> Nil)
